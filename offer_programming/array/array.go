9f4b233b8e3af1a6e43f5f677877cd1864efb390
hexiang3 <hexiang3@staff.weibo.com>
2023-04-23 22:58:06 +0800

    数组

--- offer_programming/array/array.go
+++ offer_programming/array/array.go
@@ -1,5 +1,8 @@
 package array
 
+import "sort"
+
+// 排序数组中的两个数字之和
 func TwoSum(numbers []int, target int) []int {
 	lhs := 0
 	rhs := len(numbers) - 1
@@ -15,4 +18,68 @@ func TwoSum(numbers []int, target int) []int {
 		}
 	}
 	return nil
+}
+
+// 数组中和为0的3个数字
+func ThreeSum(nums []int) [][]int {
+	sort.Slice(nums, func(i, j int) bool { return nums[i] < nums[j] })
+	var ret [][]int
+	for i, num := range nums {
+		if i == 0 || num > nums[i-1] {
+			target := -num
+			lhs := i + 1
+			rhs := len(nums) - 1
+			for lhs < rhs {
+				sum := nums[lhs] + nums[rhs]
+				if sum == target {
+					ret = append(ret, []int{num, nums[lhs], nums[rhs]})
+					lhs++
+					for lhs < rhs && nums[lhs] == nums[lhs-1] {
+						lhs++
+					}
+					rhs--
+					for lhs < rhs && nums[rhs] == nums[rhs+1] {
+						rhs--
+					}
+				} else if sum > target {
+					rhs--
+				} else {
+					lhs++
+				}
+			}
+		}
+	}
+	return ret
+}
+
+func ThreeSumV2(nums []int) [][]int {
+	sort.Slice(nums, func(i, j int) bool { return nums[i] < nums[j] })
+	lth := len(nums)
+	var res [][]int
+	for i := 0; i <= lth-3 && nums[i] <= 0; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		lhs := i + 1
+		rhs := lth - 1
+		target := 0 - nums[i]
+		for lhs < rhs {
+			if nums[rhs]+nums[lhs] == target {
+				res = append(res, []int{nums[i], nums[lhs], nums[rhs]})
+				lhs++
+				rhs--
+				for lhs < rhs && nums[lhs] == nums[lhs-1] {
+					lhs++
+				}
+				for lhs < rhs && nums[rhs] == nums[rhs+1] {
+					rhs--
+				}
+			} else if nums[rhs]+nums[lhs] > target {
+				rhs--
+			} else {
+				lhs++
+			}
+		}
+	}
+	return res
 }
---
+++ offer_programming/array/array_test.go
@@ -0,0 +1,10 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	fmt.Println(ThreeSum([]int{-1, 0, 1, 2, -1, -4}))
+}
